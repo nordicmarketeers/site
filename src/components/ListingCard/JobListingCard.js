21bd5bd87383aa3d8f82d81722e1ab57600821d6
dexterlgm <31015968+dexterlgm@users.noreply.github.com>
2026-02-26 15:57:48 +0100

    Add display extra roles in tooltip if more than one on a job listing

--- src/components/ListingCard/JobListingCard.js
+++ src/components/ListingCard/JobListingCard.js
@@ -5,6 +5,7 @@ import { IoLocationSharp } from "react-icons/io5";
 import { createSlug } from "../../util/urlHelpers";
 import noPfp from "../../assets/no_pfp.jpg";
 import { capitalize, cityCountryFormat } from "../../util/listingCardHelpers";
+import classNames from "classnames";
 
 const JobListingCard = props => {
 	const { author, classes, currentListing } = props;
@@ -17,6 +18,10 @@ const JobListingCard = props => {
 		author.profileImage?.attributes.variants["square-small"]?.url || noPfp;
 
 	const cityCountry = cityCountryFormat(publicData?.location?.address);
+
+	const [firstRole, ...restRoles] = publicData.role;
+
+	const displayRole = capitalize(firstRole.replace("_", " "));
 
 	return (
 		<NamedLink className={classes} name="ListingPage" params={{ id, slug }}>
@@ -35,12 +40,33 @@ const JobListingCard = props => {
 
 					<div className={css.tagRow}>
 						{publicData?.role?.[0] && (
-							<span className={css.tag}>
-								{capitalize(
-									publicData.role[0].replace("_", " ")
-								)}
+							<span className={classNames(css.tag, css.roleTag)}>
+								{capitalize(displayRole)}
 							</span>
 						)}
+
+						{restRoles.length > 0 && (
+							<span
+								className={classNames(
+									css.tag,
+									css.moreRolesTag
+								)}
+								aria-label={`${restRoles.length} more roles`}
+							>
+								{restRoles.length > 0 && `+${restRoles.length}`}
+							</span>
+						)}
+						{restRoles.length > 0 && (
+							<span className={css.restRolesTooltip}>
+								{restRoles.map(r => (
+									<span>
+										{capitalize(r.replace("_", " "))}
+										<br />
+									</span>
+								))}
+							</span>
+						)}
+
 						{publicData?.work_model && (
 							<span className={css.tag}>
 								{capitalize(publicData.work_model)}
